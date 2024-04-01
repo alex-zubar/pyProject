3b331562b1144e1c93a6d573db88c1691f829f04
Alexey_Zubrinkin <zubar17@gmail.com>
2024-04-01 05:39:41 +0300

    Initial

--- funcs.py
+++ funcs.py
@@ -2,5 +2,12 @@ def sum():
     a = int(input("Введите число"))
     b = int(input("Введите второе число"))
     sum = a+b
-    print (sum)
+    print(sum)
 sum()
+def minus():
+    a = int(input("Введите число"))
+    b = int(input("Введите второе число"))
+    minus = a - b
+    print(minus)
+
+minus()
